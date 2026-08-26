565589b8bf5e985a8bc1205892b9d45c7849ce3b
Octavio Augusto <oarsilva6@gmail.com>
2026-08-26 19:38:43 -0300

    Arrumando o BD

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyCPLRZmZU-c_9r7qY2Lg7jsiTkByLZTrCw",
   authDomain: "zenith-agro.firebaseapp.com",
@@ -13,9 +10,20 @@ export const firebaseConfig = {
   storageBucket: "zenith-agro.firebasestorage.app",
   messagingSenderId: "407871329650",
   appId: "1:407871329650:web:6ae7951cf611f162ca79eb",
-  measurementId: "G-1NRTD8X4JD"
+  measurementId: "G-1NRTD8X4JD",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export let analytics = null;
+
+isSupported().then((supported) => {
+  if (supported) {
+    analytics = getAnalytics(app);
+  }
+});
+
+export default app;
