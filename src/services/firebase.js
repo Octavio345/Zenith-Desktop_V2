a984b393f4b8c9fa016f507b7d5c6d1db65f9064
Octavio Augusto <oarsilva6@gmail.com>
2026-08-26 19:36:13 -0300

    Arrumando firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyD3t5Q0Z5m5qxaj21qNDb4he7mIolLlCOA",
-  authDomain: "bd-tcc-test.firebaseapp.com",
-  projectId: "bd-tcc-test",
-  storageBucket: "bd-tcc-test.firebasestorage.app",
-  messagingSenderId: "983944576946",
-  appId: "1:983944576946:web:8f630082403ee1d92aaeab",
-  measurementId: "G-DG1V88WTWE",
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPLRZmZU-c_9r7qY2Lg7jsiTkByLZTrCw",
+  authDomain: "zenith-agro.firebaseapp.com",
+  projectId: "zenith-agro",
+  storageBucket: "zenith-agro.firebasestorage.app",
+  messagingSenderId: "407871329650",
+  appId: "1:407871329650:web:6ae7951cf611f162ca79eb",
+  measurementId: "G-1NRTD8X4JD"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
