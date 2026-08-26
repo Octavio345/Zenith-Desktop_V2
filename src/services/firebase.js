df610d6ca6c87c3c137105c01559951c0b9c0d97
Octavio Augusto <oarsilva6@gmail.com>
2026-08-26 18:45:08 -0300

    Arrumando erro do BD que o meu companheiro passou errado

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-export const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "instalacao-mobile.vercel.app",
+const firebaseConfig = {
+  apiKey: "AIzaSyD3t5Q0Z5m5qxaj21qNDb4he7mIolLlCOA",
+  authDomain: "bd-tcc-test.firebaseapp.com",
   projectId: "bd-tcc-test",
   storageBucket: "bd-tcc-test.firebasestorage.app",
   messagingSenderId: "983944576946",
   appId: "1:983944576946:web:8f630082403ee1d92aaeab",
-  measurementId: "G-DG1V88WTWE"
+  measurementId: "G-DG1V88WTWE",
 };
-const app = initializeApp(firebaseConfig)
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
